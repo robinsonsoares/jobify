bc23c4312eb9ddacb6b66c9cb99b4dd5864c978c
Robinson Soares <soares.robinson@gmail.com>
2020-02-12 09:24:55 -0300

    Fixing view path

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const path = require('path')
-
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open(path.resolve(__dirname,'banco.sqlite'), { Promise })
 
